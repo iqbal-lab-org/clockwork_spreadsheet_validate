543df9d1af665ec3dd6c6dfa7b9457f1dd142ea6
Martin Hunt <martin.g.hunt@gmail.com>
2018-01-26 10:28:31 +0000

    Version bump 0.1.6

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='clockwork_validate_spreadsheet',
-    version='0.1.5',
+    version='0.1.6',
     description='Checks import spreadsheet for clockwork pipeline',
     packages = find_packages(),
     author='Martin Hunt',
